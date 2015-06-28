687238280a9a703071fed00e748f07e98ec21489
nasty007 <lazic.josip@gmail.com>
2015-06-28 22:54:53 +0200

    fix NFC build

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,6 +116,9 @@ BOARD_BLUEDROID_VENDOR_CONF := device/lge/g2-common/bluetooth/vnd_g2.txt
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
 
+# NFC
+TARGET_USES_OS_NFC := true
+
 # Offmode Charging
 BOARD_CHARGER_ENABLE_SUSPEND := true
 COMMON_GLOBAL_CFLAGS += \
